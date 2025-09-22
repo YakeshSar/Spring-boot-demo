27ab4a279feef4d2efdfb9fecb550e00cba737c5
YakeshSar <93547089+YakeshSar@users.noreply.github.com>
2025-09-22 23:12:04 +0530

    Add sb project

---
+++ FunRestAPi/src/main/java/com/example/FunRestAPi/FunRestAPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FunRestAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunRestAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunRestAPiApplication.class, args);
+	}
+
+}
---
+++ FunRestAPi/src/test/java/com/example/FunRestAPi/FunRestAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FunRestAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunRestAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
